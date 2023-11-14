51cec816d615d4679686b1ecfefb9914c4fa2529
bnassim777 <bnassim750@gmail.com>
2023-11-14 14:08:14 +0100

    deuxieme partie du test

--- _test_/app.test.js
+++ _test_/app.test.js
@@ -1,10 +1,29 @@
+// // const app = require('../app.js');
+
+// // describe('test',() => {
+// //     it('should retrieve a sentence',() => {
+
+// //         expect(app.sentences.length).toBeGreaterThan(0);
+// //     });
+
+
+// });
+
+
 const app = require('../app.js');
 
-describe('test',() => {
-    it('should retrieve a sentence',() => {
-
-        expect(app.sentences.length).toBeGreaterThan(0);
+describe('Palindrom',() => {
+    it('should be a Palindrom',() => {
+        expect(app.isPalindrom('kayak')).toEqual(true);
     });
-
-
 });
+
+
+describe('moderator',() => {
+    it('contient des mots interdits',() => {
+        expect(app.containForbiddenWords('vous etes des nuls')).toEqual(true);
+    });
+    it.todo('test suivant à réaliser');
+    it.todo('test à réaliser plus tard');
+})
+
--- app.js
+++ app.js
@@ -1,5 +1,42 @@
-const sentence = "hello world"
+// const sentence = "hello world"
+
+// module.exports = {
+//     sentence
+// }
+
+function isPalindrom(word) {
+    let wordle = word
+    let reverseWord = wordle.split('').reverse().join('');
+    return reverseWord === word;
+};
+
+
+
+
+
+const forbiddenWords = ['nuls', 'nul', 'imbécile', 'imbeciles', 'idiots', 'idiot', 'débile', 'debiles'];
+
+function containForbiddenWords(message) {
+
+    const msg=message.split(' ');
+    let result = [];
+    for (let index = 0; index < msg.length; index++) {
+        const item = msg[index];
+        if (forbiddenWords.includes(item)) {
+            result.push(item);
+        }
+
+      
+    }  
+    if (result.length > 0) {
+            return true;
+        }
+        return false;
+}
+
+
 
 module.exports = {
-    sentence
+    isPalindrom,
+    containForbiddenWords
 }
